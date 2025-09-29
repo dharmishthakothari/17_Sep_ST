f97bf54c55409b4f1a76231940750499fb64ef95
dharmishthakothari <dharmishtha.only@gmail.com>
2025-09-29 10:04:04 +0530

    Add files via upload

---
+++ ArithmaticOperator.java
@@ -0,0 +1,23 @@
+package basic;
+
+public class ArithmaticOperator {
+
+	public static void main(String[] args) {
+		int number1=100,number2=20;
+		System.out.println("Addition "+number1+number2);
+		System.out.println(number1+number2);
+		int add=number1+number2;
+		System.out.println("Addition "+add);
+		int sub=number1-number2;
+		System.out.println("Substraction "+sub);
+		int mul=number1*number2;
+		System.out.println("Multiplication "+mul);
+		int div=number1/number2;
+		System.out.println("Division "+div);
+		// % gives you reminder 
+		int mod=number1%number2;
+		System.out.println("Modulus "+mod);
+		
+	}
+
+}
---
+++ RelationalOpe.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class RelationalOpe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1=100,number2=50;
+		System.out.println(number1>number2);
+		System.out.println(number1<number2);
+	}
+
+}
