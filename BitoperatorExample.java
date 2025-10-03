7d6864c1ae55d7b2f83f397835fa4030f76230c2
dharmishthakothari <dharmishtha.only@gmail.com>
2025-10-03 10:05:14 +0530

    Add files via upload

---
+++ BitoperatorExample.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class BitoperatorExample {
+		
+public static void main(String[] args) {
+	int number1=5,number2=20;
+	System.out.println("5&20 =  "+(5&20));
+	System.out.println("5|20 = " +(5|20));
+	
+	System.out.println("5<<2 = "+(5<<2));
+	System.out.println("5>>2 = "+(5>>2));	
+	System.out.println(" 5^2 = "+(5^2));
+	
+	
+}
+}
---
+++ TernaryOperator.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int number=1000,number2=200;
+			int ans;
+			ans=number>number2?number:number2;
+			System.out.println(ans);
+	}
+
+}
